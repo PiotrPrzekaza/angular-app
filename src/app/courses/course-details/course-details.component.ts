42634572afc831927189d83365b4a40e3064fa57
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-27 19:56:01 +0200

    make detalis component

--- src/app/courses/course-details/course-details.component.ts
+++ src/app/courses/course-details/course-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.scss']
+  styleUrls: ['./course-details.component.scss'],
 })
 export class CourseDetailsComponent implements OnInit {
+  @Input() currentCourse;
+  @Output() save = new EventEmitter();
+  @Output() cancel = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
